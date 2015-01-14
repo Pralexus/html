0303ff26a9c4158fb1c8d51e808e47f784f0159c
Pralexus <prus.a.wezom@gmail.com>
2015-01-14 15:42:53 +0200

    fix email valid

--- js/plugins.js
+++ js/plugins.js
@@ -1370,7 +1370,7 @@
         },
 
         email: function( value, element ) {
-            return this.optional( element ) || /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test( value );
+            return this.optional( element ) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test( value );
         },
 
         // http://jqueryvalidation.org/url-method/
