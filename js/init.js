1dc81376f310f131656d5308373d5a71425e061a
Pralexus <prus.a.wezom@gmail.com>
2015-01-30 18:04:14 +0200

    remove valid init

--- js/init.js
+++ js/init.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
     
-    if ($('.validat').length) {
-        $('.validat').liValidForm({
-            captcha: 'code'
-        });
-    }
-
     // detect transit support
     /*var transitFlag = true;
     if (!Modernizr.cssanimations) {
