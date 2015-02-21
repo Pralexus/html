3028d9447ab2444ab04dc89a493a73ec09a0e100
pralexus <prus.a.wezom@gmail.com>
2015-02-21 14:25:27 +0200

    fix

--- js/init.js
+++ js/init.js
@@ -27,7 +27,40 @@ $(document).ready(function() {
                 if (form.tagName === 'FORM') {
                     form.submit();
                 } else {
-                    
+                    var ajUrl = $(form).attr('data-form-url'); /* path/to/file.php */
+                    var ajData = $(form).find('select, textarea, input').serializeArray();
+                    console.log(ajData);
+                    $.ajax({
+                        type: 'POST',
+                        url: ajUrl,
+                        data: ajData,
+                        dataType: 'json',
+                        success: function(data) {
+                            // очистка полей формы
+                            /*$(':input', form).not(':button, :submit, :reset, :hidden').val('').removeAttr('checked').removeAttr('selected');*/
+
+                            // перезагрузка страницы
+                            /*window.location.reload();*/
+
+                            //сообщение
+                            /*$.magnificPopup.open({
+                                modal: true,
+                                preloader: false,
+                                items: {
+                                    src: '<div class="modalMagnific zoomAnim"><p>'+data.message+'</p></div>',
+                                    type: 'inline'
+                                },
+                                removalDelay: 300,
+                                mainClass: 'zoom-in'
+                            });
+                            setTimeout(function() {
+                                $.magnificPopup.close();
+                            }, 3000);*/
+                        },
+                        error: function() {
+                            console.error('Что то пошло не так!');
+                        }
+                    });
                 }
             }
         });
@@ -42,4 +75,20 @@ $(document).ready(function() {
         }
     });
 
+    $('.enterReg').magnificPopup({
+        type: 'inline',
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'zoom-in'
+    });
+
+    $('#forget_pass').on('click', function(event) {
+        $('#entrForm').removeClass('visForm');
+        $('#forgetForm').addClass('visForm');
+    });
+
+    $('#remember_pass').on('click', function(event) {
+        $('#forgetForm').removeClass('visForm');
+        $('#entrForm').addClass('visForm');
+    });
 });
